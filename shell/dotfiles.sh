5060a55460f438a51e20d1f15f21d833ae684d1d
Richard Banyi <rbanyi@me.com>
2020-03-07 23:49:19 +0100

    fix link for dotfiles

--- shell/dotfiles.sh
+++ shell/dotfiles.sh
@@ -4,12 +4,13 @@ bot ".dotfiles"
 
 read -r -p "Do you want me to install dotfiles? [y|N] " response
 if [[ $response =~ (yes|y|Y) ]];then
-    bot "Installing dotfiles"
-    git clone --recursive https://github.com/richban/setupbot-osx-linux.git "$HOME"/.dotfiles
+    bot "Cloninng dotfiles"
+    git clone --recursive https://github.com/richban/dotfiles.git "$HOME"/.dotfiles
     cd "$HOME"/.dotfiles || return
-    action "installing dotdrop manager"
+    action "Installing dotdrop manager"
     pip3 install --user -r "$HOME"/.dotfiles/dotdrop/requirements.txt
-    action "installing dotfiles"
+    ./dotdrop/bootstrap.sh
+    action "Installing dotfiles"
     read -r -p "Which profile wish you to install?" profile
     ./dotdrop.sh install --profile="$profile"
     ok
